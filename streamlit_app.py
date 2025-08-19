6a938752ee5f744f843a6f3f31d09977e551a25d
Derya Gumustel <degumustel@gmail.com>
2025-08-19 18:49:07 +0000

    adding blank streamlit app page

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Riptide Remix")
+st.write(
+    "This is the homepage for the Riptide Remix project at OHW 2025! Check out our [GitHub Repo](https://github.com/oceanhackweek/ohw25_proj_RiptideRemix)."
+)
